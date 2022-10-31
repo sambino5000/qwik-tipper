74474f1671d426a29fe405708cd64f8dbc7c86d5
samrock <samnev86@gmail.com>
2022-10-31 00:29:55 -0500

    add cloudflare pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import { qwikVite } from '@builder.io/qwik/optimizer';
-import { qwikCity } from '@builder.io/qwik-city/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
+    ssr: { target: "webworker", noExternal: true },
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
-    optimizeDeps:{
-      include:['lean-qr','chronik-client','@samrock5000/cashscript',]//'@bitauth/libauth'
-    }
+    optimizeDeps: {
+      include: ["lean-qr", "chronik-client", "@samrock5000/cashscript"], //'@bitauth/libauth'
+    },
   };
 });
